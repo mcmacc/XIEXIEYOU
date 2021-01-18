de7ce2a90e46d9737653758d960188e6e880060f
zhizhi945 <77368638+zhizhi945@users.noreply.github.com>
2021-01-18 13:01:01 +0800

    Update smartReplace.js

--- smartReplace.js
+++ smartReplace.js
@@ -14,6 +14,10 @@ async function inject() {
 
 async function inject_jd() {
     if (!process.env.JD_COOKIE) return;
+    replacements.push({
+        key: "GITHUB",
+        value: `DONOTUSEACTION`,
+    });
     if (remoteContent.indexOf("function requireConfig()") >= 0 && remoteContent.indexOf("jd_bean_sign.js") >= 0) {
         replacements.push({
             key: "resultPath = err ? '/tmp/result.txt' : resultPath;",
@@ -163,68 +167,68 @@ function batchReplace() {
 
 async function downloader_jd() {
     if (/require\(['"`]{1}.\/jdCookie.js['"`]{1}\)/.test(remoteContent))
-        await download("https://github.com/Choicc/MyActions/raw/main/scripts/jdCookie.js", "./jdCookie.js", "京东Cookies");
+        await download("https://gitee.com/lxk0301/jd_scripts/raw/master/jdCookie.js", "./jdCookie.js", "京东Cookies");
     if (remoteContent.indexOf("jdFruitShareCodes") > 0) {
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/jdFruitShareCodes.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/jdFruitShareCodes.js",
             "./jdFruitShareCodes.js",
             "东东农场互助码"
         );
     }
     if (remoteContent.indexOf("jdPetShareCodes") > 0) {
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/jdPetShareCodes.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/jdPetShareCodes.js",
             "./jdPetShareCodes.js",
             "京东萌宠"
         );
     }
     if (remoteContent.indexOf("jdPlantBeanShareCodes") > 0) {
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/jdPlantBeanShareCodes.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/jdPlantBeanShareCodes.js",
             "./jdPlantBeanShareCodes.js",
             "种豆得豆互助码"
         );
     }
     if (remoteContent.indexOf("jdSuperMarketShareCodes") > 0)
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/jdSuperMarketShareCodes.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/jdSuperMarketShareCodes.js",
             "./jdSuperMarketShareCodes.js",
             "京小超互助码"
         );
     if (remoteContent.indexOf("jdFactoryShareCodes") > 0) {
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/jdFactoryShareCodes.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/jdFactoryShareCodes.js",
             "./jdFactoryShareCodes.js",
             "东东工厂互助码"
         );
     }
     if (remoteContent.indexOf("jdDreamFactoryShareCodes") > 0) {
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/jdDreamFactoryShareCodes.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/jdDreamFactoryShareCodes.js",
             "./jdDreamFactoryShareCodes.js",
             "京喜工厂互助码"
         );
     }
     if (remoteContent.indexOf("jdJxncTokens.js") > 0) {
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/jdJxncTokens.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/jdJxncTokens.js",
             "./jdJxncTokens.js",
             "京喜农场token"
         );
     }
     if (remoteContent.indexOf("new Env('京喜农场')") > 0) {
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/jdJxncTokens.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/jdJxncTokens.js",
             "./jdJxncTokens.js",
             "京喜农场Token"
         );
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/jdJxncShareCodes.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/jdJxncShareCodes.js",
             "./jdJxncShareCodes.js",
             "京喜农场分享码"
         );
         await download(
-            "https://github.com/Choicc/MyActions/raw/main/scripts/USER_AGENTS.js",
+            "https://gitee.com/lxk0301/jd_scripts/raw/master/USER_AGENTS.js",
             "./USER_AGENTS.js",
             "USER_AGENTS"
         );
@@ -232,16 +236,17 @@ async function downloader_jd() {
 }
 
 async function downloader_notify() {
-    await download("https://github.com/Choicc/MyActions/raw/main/scripts/sendNotify.js", "./sendNotify.js", "统一通知");
+    await download("https://gitee.com/lxk0301/jd_scripts/raw/master/sendNotify.js", "./sendNotify.js", "统一通知");
 }
 
 async function downloader_user_agents() {
-    await download("https://github.com/Choicc/MyActions/raw/main/scripts/USER_AGENTS.js", "./USER_AGENTS.js", "云端UA");
+    await download("https://gitee.com/lxk0301/jd_scripts/raw/master/USER_AGENTS.js", "./USER_AGENTS.js", "云端UA");
 }
 
 async function download(url, path, target) {
     let response = await axios.get(url);
     let fcontent = response.data;
+    if(process.env.REPOURL) fcontent = fcontent.replace('GITHUB','DONOTUSEACTION');
     await fs.writeFileSync(path, fcontent, "utf8");
     console.log(`下载${target}完毕`);
 }
